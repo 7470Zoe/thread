de0c1c2977613928c7dd5ca3e08fb12d5095c890
syt <m13058178050@163.com>
2021-07-06 11:32:37 +0800

    volatile不能保证 原子性

---
+++ src/main/java/org/example/TestVolatile.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestVolatile {
+    /*volatile */int count = 0;
+    public synchronized void m(){
+        for (int i = 0; i < 10000; i++) {
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+        TestVolatile t = new TestVolatile();
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            threads.add(new Thread(t::m,"thread-"+i));
+        }
+        threads.forEach((e)->e.start());
+        threads.forEach((e)-> {
+            try {
+                e.join();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        });
+        System.out.println(t.count);
+    }
+
+    /**
+     * 一共10个线程,每个线程执行一万次的count++,期望结果是得到10万,没有synchronized不会保证线程执行的原子性,得不到10万
+     * volatile只能是保证线程可见性,不能保证原子性
+     */
+}
