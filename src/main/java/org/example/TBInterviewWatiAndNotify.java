8ca6e444214bc78c7038bc56b514ec0bf6903ad5
syt <m13058178050@163.com>
2021-07-08 18:20:01 +0800

    TB面试题

--- src/main/java/org/example/TBInterviewWatiAndNotify.java
+++ src/main/java/org/example/TBInterviewWatiAndNotify.java
@@ -6,8 +6,24 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 使线程2监控线程1,当线程1中数据有5个的时候,线程2结束执行
+ *
+ * 曾经的面试题：（淘宝？）
+ * 实现一个容器，提供两个方法，add，size
+ * 写两个线程，线程1添加10个元素到容器中，线程2实现监控元素的个数，当个数到5个时，线程2给出提示并结束
+ *
+ * 给lists添加volatile之后，t2能够接到通知，但是，t2线程的死循环很浪费cpu，如果不用死循环，该怎么做呢？
+ *
+ * 这里使用wait和notify做到，wait会释放锁，而notify不会释放锁
+ * 需要注意的是，运用这种方法，必须要保证t2先执行，也就是首先让t2监听才可以
+ *
+ * 阅读下面的程序，并分析输出结果
+ * 可以读到输出结果并不是size=5时t2退出，而是t1结束时t2才接收到通知而退出
+ * 想想这是为什么？
+ *
+ * notify之后，t1必须释放锁，t2退出后，也必须notify，通知t1继续执行
+ * 整个通信过程比较繁琐
  */
-public class TBInterviewgood03 {
+public class TBInterviewWatiAndNotify {
     //添加volatile，使t2能够得到通知
     volatile List lists = new ArrayList();
 
@@ -21,7 +37,7 @@ public class TBInterviewgood03 {
 
     public static void main(String[] args) {
         final Object o = new Object();
-        TBInterviewgood03 tbInterviewgood03 = new TBInterviewgood03();
+        TBInterviewWatiAndNotify tbInterviewgood03 = new TBInterviewWatiAndNotify();
         new Thread(() -> {
             synchronized (o) {
                 System.out.println("线程2启动");
